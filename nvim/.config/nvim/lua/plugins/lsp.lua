af96209a717166ad06a16ab0a264464ae5fa0ef9
Muhammad Ahmad <m.ahmad00861@gmail.com>
2026-05-29 17:28:21 +0100

    Update mason.nvim to mason-org/mason.nvim

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 -- Extra Mason tools not covered by LazyVim lang extras
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
